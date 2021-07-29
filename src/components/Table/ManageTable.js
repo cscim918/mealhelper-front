24544948a68c4d0e4110a4316f9707b454706848
Yunbo <cscim918@gmail.com>
2021-07-29 15:56:38 +0900

    table update

--- src/components/Table/ManageTable.js
+++ src/components/Table/ManageTable.js
@@ -1,52 +1,52 @@
-import React from "react";
-import { useTable } from "react-table";
-import useData from "./useData";
-import useColumns from "./useColumns";
+import React from 'react';
+import { useTable } from 'react-table';
+import useData from './useData';
+import useColumns from './useColumns';
 import './ManageTable.css';
 
 function ManageTable() {
-  const data = useData();
-  const columns = useColumns();
-
-  const {
-    getTableProps,
-    getTableBodyProps,
-    headerGroups,
-    rows,
-    prepareRow
-  } = useTable({ columns, data });
-
   return (
-    <div className="container">
-    <h4>하루 권장 칼로리</h4>
-    <br/>
-      <table {...getTableProps()}>
-        <thead>
-          {headerGroups.map(headerGroup => (
-            <tr {...headerGroup.getHeaderGroupProps()}>
-              {headerGroup.headers.map(column => (
-                <th {...column.getHeaderProps()}>{column.render("Header")}</th>
-              ))}
-            </tr>
-          ))}
-        </thead>
-        <tbody {...getTableBodyProps()}>
-          {rows.map(row => {
-            prepareRow(row);
-
-            return (
-              <tr {...row.getRowProps()}>
-                {row.cells.map(cell => {
-                  return (
-                    <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
-                  );
-                })}
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    </div>
+    <table>
+      <tr>
+        <th>Age</th>
+        <td>6-8</td>
+        <td>9-11</td>
+        <td>12-14</td>
+        <td>15-18</td>
+        <td>19-29</td>
+        <td>30-39</td>
+        <td>40-49</td>
+        <td>50-64</td>
+        <td>65-74</td>
+        <td>75이상</td>
+      </tr>
+      <tr>
+        <th>Man</th>
+        <td>1700</td>
+        <td>2100</td>
+        <td>2500</td>
+        <td>2700</td>
+        <td>2600</td>
+        <td>2400</td>
+        <td>2400</td>
+        <td>2200</td>
+        <td>2000</td>
+        <td>2000</td>
+      </tr>
+      <tr>
+        <th>Woman</th>
+        <td>1500</td>
+        <td>1800</td>
+        <td>2000</td>
+        <td>2000</td>
+        <td>2100</td>
+        <td>1900</td>
+        <td>1900</td>
+        <td>1800</td>
+        <td>1600</td>
+        <td>1600</td>
+      </tr>
+    </table>
   );
 }
 
