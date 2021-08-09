e3949ff7d376e7de25c05638feb4a59ab14405f9
Yunbo <cscim918@gmail.com>
2021-08-09 13:00:03 +0900

    mealhelper-front ver1.0.2

--- src/utils/http-common.js
+++ src/utils/http-common.js
@@ -1,7 +1,9 @@
 import axios from 'axios'
+import dotenv from 'dotenv';
+dotenv.config();
 
 export default axios.create({
-    baseURL: 'http://ec2-3-34-253-134.ap-northeast-2.compute.amazonaws.com:5000/api',
+    baseURL: process.env.BASE_URL,
     headers: {
         "Content-type": "application/json"
     }
